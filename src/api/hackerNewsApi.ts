bc51da17032acee21e0866ccb6285b14282384da
Spyros Bekiaris <spirosbekos@gmail.com>
2024-11-07 14:55:10 +0200

    Create StoryData interface, Story class and its corresponding constructor

--- src/api/hackerNewsApi.ts
+++ src/api/hackerNewsApi.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { Story, StoryData } from '../types/Story';
 
 // Base URL for the Hacker News API
 const BASE_URL = process.env.API_URL || 'https://hn.algolia.com/api/v1';
@@ -10,7 +11,7 @@ export const searchStories = async (query: string) => {
                 query,
             },
         });
-        return response.data.hits;
+        return response.data.hits.map((storyData: StoryData) => new Story(storyData));
     } catch (error) {
         console.error('Error fetching stories:', error);
         throw error;
---
+++ src/types/Story.ts
@@ -0,0 +1,26 @@
+export interface StoryData {
+    objectID: string;
+    url: string;
+    title: string;
+    author: string;
+    points?: number;
+    num_comments?: number;
+}
+
+export class Story {
+    id: string;
+    url: string;
+    title: string;
+    author: string;
+    points?: number;
+    commentsNumber?: number;
+
+    constructor(data: StoryData) {
+        this.id = data.objectID;
+        this.title = data.title;
+        this.url = data.url;
+        this.author = data.author;
+        this.points = data.points;
+        this.commentsNumber = data.num_comments;
+    }
+}
